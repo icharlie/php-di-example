8837514df6c25e10158c1ed8aa6b9f46189d56c5
Charlie <icharlie.osu@gmail.com>
2015-02-21 17:45:01 -0800

    Move db query duty to repository class.

---
+++ service/PopulationRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Class PopulationRepository
+ * @author Charlie Chang
+ */
+class PopulationRepository
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO(
+                'mysql:host=127.0.0.1;dbname=world;port=3306;charset=utf8',
+                'homestead',
+                'secret'
+            );
+
+        } catch(PDOException $e) {
+            die(json_encode(array('error' => 'y', 'message' => 'Database connection failed')));
+        }
+    }
+
+    public function queryTopPopulationCities($top)
+    {
+        $results = array();
+
+
+        $sql = 'SELECT Name, Population FROM City ORDER BY Population DESC limit :top';
+        $statment = $this->pdo->prepare($sql);
+        $statment->bindValue(':top', $top, PDO::PARAM_INT);
+        $statment->execute();
+
+        while(($result = $statment->fetch(PDO::FETCH_ASSOC)) !== false) {
+            $results[] = $result;
+        }
+
+        return $results;
+
+    }
+}
--- service/population.php
+++ service/population.php
@@ -8,32 +8,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
  */
 class Population
 {
+    public function __construct(PopulationRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function getTopPopulationCities($top)
     {
-        $results = array();
-
-        try {
-            $pdo = new PDO(
-                'mysql:host=127.0.0.1;dbname=world;port=3306;charset=utf8',
-                'homestead',
-                'secret'
-            );
-
-        } catch(PDOException $e) {
-            die(json_encode(array('error' => 'y', 'message' => 'Database connection failed')));
-        }
-
-
-        $sql = 'SELECT Name, Population FROM City ORDER BY Population DESC limit :top';
-        $statment = $pdo->prepare($sql);
-        $statment->bindValue(':top', $top, PDO::PARAM_INT);
-        $statment->execute();
-
-        while(($result = $statment->fetch(PDO::FETCH_ASSOC)) !== false) {
-            $results[] = $result;
-        }
-
-        return $results;
+        return $this->repository->queryTopPopulationCities($top);
     }
 }
 
