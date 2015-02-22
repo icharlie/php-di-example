a401de3512cd43a42efd5a498124a76967e4d5d7
Charlie <icharlie.osu@gmail.com>
2015-02-21 17:52:56 -0800

    Mock PopulationRepository and Pass the test.

--- tests/PopulationTest.php
+++ tests/PopulationTest.php
@@ -1,20 +1,32 @@
 <?php
 
+use Mockery as m;
+
 /**
  * Class PopulationTest
  * @author Charlie Chang
  */
 class PopulationTest extends TestCase
 {
-	public function setUp()
-	{
-		parent::setUp();
-		$this->population = $this->container->make('Population');
-	}
+    public function setUp()
+    {
+        parent::setUp();
+		$this->populationRepo = m::mock('PopulationRepository');
+        $this->container->set('PopulationRepository', $this->populationRepo);
+        $this->population = $this->container->make('Population');
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
 
     public function testGetTopPopulationCities()
     {
-		$result = $this->population->getTopPopulationCities(10);
-		$this->assertEquals([['Name' => 'A', 'Population' => 100]], $result);
+		$this->populationRepo->shouldReceive('queryTopPopulationCities')
+			->once()->andReturn([['Name' => 'A', 'Population' => 100]]);
+
+        $result = $this->population->getTopPopulationCities(10);
+        $this->assertEquals([['Name' => 'A', 'Population' => 100]], $result);
     }
 }
