108ad6a86ff33a595dbcedd3450757b05f53180c
Charlie <icharlie.osu@gmail.com>
2015-02-21 17:20:00 -0800

    Using APP_ENV environment variable to let test skip script part.

--- service/population.php
+++ service/population.php
@@ -13,42 +13,46 @@ class Population
     }
 }
 
-$action = filter_input(INPUT_GET, 'action');
-$top = (int)filter_input(INPUT_GET, 'top');
 
-try {
-    $pdo = new PDO(
-        'mysql:host=127.0.0.1;dbname=world;port=3306;charset=utf8',
-        'homestead',
-        'secret'
-    );
+if (getenv('APP_ENV') != 'testing') {
+    $action = filter_input(INPUT_GET, 'action');
+    $top = (int)filter_input(INPUT_GET, 'top');
 
-} catch(PDOException $e) {
-    die(json_encode(array('error' => 'y', 'message' => 'Database connection failed')));
-}
+    try {
+        $pdo = new PDO(
+            'mysql:host=127.0.0.1;dbname=world;port=3306;charset=utf8',
+            'homestead',
+            'secret'
+        );
 
-$results = array();
-if ($action == 'getTopPopulationCities') {
-
-    $sql = 'SELECT Name, Population FROM City ORDER BY Population DESC limit :top';
-    $statment = $pdo->prepare($sql);
-    $statment->bindValue(':top', $top, PDO::PARAM_INT);
-    $statment->execute();
-
-    while(($result = $statment->fetch(PDO::FETCH_ASSOC)) !== false) {
-        $results[] = $result;
+    } catch(PDOException $e) {
+        die(json_encode(array('error' => 'y', 'message' => 'Database connection failed')));
     }
-    die(json_encode($results));
-}
 
-if ($action == 'getTopPopulationCountries') {
-    $sql = 'SELECT Name, Population FROM Country ORDER BY Population DESC limit :top';
-    $statment = $pdo->prepare($sql);
-    $statment->bindValue(':top', $top, PDO::PARAM_INT);
-    $statment->execute();
+    $results = array();
+    if ($action == 'getTopPopulationCities') {
 
-    while(($result = $statment->fetch(PDO::FETCH_ASSOC)) !== false) {
-        $results[] = $result;
+        $sql = 'SELECT Name, Population FROM City ORDER BY Population DESC limit :top';
+        $statment = $pdo->prepare($sql);
+        $statment->bindValue(':top', $top, PDO::PARAM_INT);
+        $statment->execute();
+
+        while(($result = $statment->fetch(PDO::FETCH_ASSOC)) !== false) {
+            $results[] = $result;
+        }
+        die(json_encode($results));
     }
-    die(json_encode($results));
+
+    if ($action == 'getTopPopulationCountries') {
+        $sql = 'SELECT Name, Population FROM Country ORDER BY Population DESC limit :top';
+        $statment = $pdo->prepare($sql);
+        $statment->bindValue(':top', $top, PDO::PARAM_INT);
+        $statment->execute();
+
+        while(($result = $statment->fetch(PDO::FETCH_ASSOC)) !== false) {
+            $results[] = $result;
+        }
+        die(json_encode($results));
+    }
 }
+
