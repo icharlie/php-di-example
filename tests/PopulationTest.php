72661b17dce11c874c830940496051892951be2d
Charlie <icharlie.osu@gmail.com>
2015-02-21 17:01:04 -0800

    First testing

---
+++ tests/PopulationTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class PopulationTest
+ * @author Charlie Chang
+ */
+class PopulationTest extends TestCase
+{
+    public function testGetTopPopulationCities()
+    {
+        $this->assertTrue(true);
+    }
+}
